efa1694fd112dd625776e2b444afedfd01875ddf
Henry Millison <hmillison@gmail.com>
2017-04-29 17:33:40 -0500

    update lib exports

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict'
 
-const { sagaTestEngine } = require('./src')
-module.exports = sagaTestEngine
+const {
+    collectPuts,
+    collectCalls,
+    collectCallsAndPuts,
+    createSagaTestEngine
+} = require('./src')
+
+exports.default = collectPuts
+exports.collectCalls = collectCalls
+exports.collectCallsAndPuts = collectCallsAndPuts
+exports.createSagaTestEngine = createSagaTestEngine
