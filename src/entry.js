b8c37147b18b8673bee94f3f34dd791ba87a7c7e
Henry Millison <hmillison@gmail.com>
2017-05-05 14:44:07 -0500

    update import

--- src/entry.js
+++ src/entry.js
@@ -1,11 +1,11 @@
 'use strict'
 
 const {
-  collectPuts,
-  collectCalls,
-  collectCallsAndPuts,
-  createSagaTestEngine,
-} = require('./src')
+    collectPuts,
+    collectCalls,
+    collectCallsAndPuts,
+    createSagaTestEngine,
+} = require('./index')
 
 module.exports = {
   default: createSagaTestEngine,
